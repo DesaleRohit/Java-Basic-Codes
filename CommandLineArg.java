156a4f66392c8ea187378d8b359667d15ec7a4e5
DesaleRohit <rohitdesale2005@gmail.com>
2025-01-07 11:00:09 +0530

    [ADDED]:ROhit -Demonstration of Command Line argument

---
+++ CommandLineArg.java
@@ -0,0 +1,18 @@
+class CommandLineArg
+{
+  public static void main(String []args)
+  {
+   // System.out.println(args[0]);
+   // System.out.println(args[1]);
+   // System.out.println(args[2]);
+   // System.out.println(args[3]);
+   // System.out.println(args[4]); 
+   
+
+    for(int i=0; i<args.length ; i++)
+    {
+      System.out.println(args[i]);
+    }
+  
+  }
+}
