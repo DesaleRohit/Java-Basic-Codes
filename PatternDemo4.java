7e5aadc6df110f3ffca6d6825e18ebb98d78b1b2
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-06 22:18:02 +0530

    [ADDED]:Rohit - This program for Pattren Printing

---
+++ PatternDemo4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Patterndemo4 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the Number: ");
+        int n = sc.nextInt();
+        
+        for(int r = 1; r <= n; r++) {
+            for(int c = 1; c <= n; c++) {
+                System.out.print(r + " ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
