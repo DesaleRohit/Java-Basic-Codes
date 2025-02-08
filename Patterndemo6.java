f41451c18c8a01daecb2bd1c7f8ad307d39fd866
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-08 21:34:02 +0530

    [ADDED]:Rohit - This program prints Horizontal Right Angle triangle

---
+++ Patterndemo6.java
@@ -0,0 +1,15 @@
+class Patterndemo6 
+{
+    public static void main(String[] args) 
+    {
+        int n = 10;
+        
+        for (int r = 1; r <= n; r++) {
+            for (int c = 1; c <= n - r + 1; c++) 
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
