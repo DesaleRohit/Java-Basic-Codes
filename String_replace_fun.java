c736994d7906dab61446abc71a196ba4b4bbf7ec
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-09 17:35:23 +0530

    [ADDED]:Rohit - This program for String Replace()

---
+++ String_replace_fun.java
@@ -0,0 +1,10 @@
+class String_replace_fun
+{
+	public static void main(String args[])
+	{
+
+		String s = "Imrd College Shirpur";
+		System.out.println("replace() = "+s.replace("Shirpur" , "Shahada"));
+
+	}
+}
