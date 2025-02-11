1baefb2e41f1edcdbd40420be2f5ca2fb1bd02a6
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-11 09:57:41 +0530

    [ADDED]:Rohit - This program for String startwith()

---
+++ String_start_fun.java
@@ -0,0 +1,11 @@
+class String_start_fun
+{
+	public static void main(String args[]) {
+
+		String a = "Rohit";
+
+		System.out.println("startsWith() = "+ a.startsWith("Ro"));
+		System.out.println("startsWith() = "+ a.startsWith("hi"));
+		System.out.println("startsWith() = "+ a.startsWith("R"));
+	}
+}
