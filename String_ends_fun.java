2110a0c4e6503d87f2526c2ca90ba4537f4e9bfa
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-12 09:17:17 +0530

    [ADDED]:Rohit - This program for Stirng endsWith()

---
+++ String_ends_fun.java
@@ -0,0 +1,10 @@
+class Main 
+{
+    public static void main(String[] args) 
+    {
+        String a = "Rohit Desale";
+        
+        System.out.println("endsWith() = "+a.endsWith("le"));
+        System.out.println("endsWith() = "+a.endsWith("De"));
+    }
+}
