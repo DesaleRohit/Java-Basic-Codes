50d3b580537c42aaee55d379fd968970eebeec9b
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-13 08:54:32 +0530

    [ADDED]:Rohit - This program for String equals()

---
+++ String_equ_fun.java
@@ -0,0 +1,14 @@
+class String_equ_fun
+{
+	public static void main(String args[]) 
+	{
+
+		String a = "IMRD";
+		String b = "IMRD";
+		String c = "College";
+
+		System.out.println("String equals = "+a.equals(b));
+		System.out.println("String not equals = "+a.equals(c));
+
+	}
+}
