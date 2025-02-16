bbc480dc061c0b1a90d62cc14c1b1d25be5303fb
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-16 21:54:45 +0530

    [ADDED]:ROhit - This program prints Hollow Triangle

---
+++ Patterndemo11.java
@@ -0,0 +1,25 @@
+class Patterndemo11
+{
+    public static void main(String[] args) 
+    {
+        int n = 8;
+
+        for (int r = 1; r <= n; r++) 
+        {
+            for (int c = 1; c <= r; c++) 
+            {
+                if (c == r || c <= 1 || r == n)
+                {
+                 System.out.print("* ");
+                }
+                   
+                else
+                {
+                 System.out.print("  ");
+                }
+                   
+            }
+            System.out.println();
+        }
+    }
+}
