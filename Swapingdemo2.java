ddc1a6d9815017f2a938fb06f9f8d4bb941a28e6
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-26 21:38:51 +0530

    [ADDED]:Rohit - This program perform Swaping without Third variable

---
+++ Swapingdemo2.java
@@ -0,0 +1,18 @@
+class SwapingDemo2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Before Swaping : ");
+        System.out.println("A = "+a);
+        System.out.println("B = "+b);
+
+        a = a - b;
+        b = a + b;
+        a = b - a;
+
+        System.out.println("After Swaping : ");
+        System.out.println("A = "+a);
+        System.out.println("B = "+b);
+
+    }
+}
