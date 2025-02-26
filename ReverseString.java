9af88c5e8342a2910d75b0d3fb284a3d08038d0f
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-26 15:25:42 +0530

    [ADDED]:Rohit - This program for Reverse String

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+class ReverseString 
+{
+    public static void main(String[] args) 
+    {
+       String a="Rohit";
+       
+       for(int i=4 ; i>=0 ; i--)
+       {
+           System.out.print(a.charAt(i)+"  ");
+       }
+    }
+}
