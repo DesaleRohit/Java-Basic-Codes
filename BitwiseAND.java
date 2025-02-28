7a49795a650b3c7bf37b9e267fdefebb6093b9db
DesaleRohit <rohitdesale2005@gmail.com>
2025-02-28 21:14:37 +0530

    [ADDED]:Rohit - This code for Bitwise AND

---
+++ BitwiseAND.java
@@ -0,0 +1,10 @@
+class BitwiseAND {
+    public static void main(String[] args) {
+        int a = 5;                  //Binary 101
+        int b = 4;                  //Binary 100          
+        int c = a & b;            //---------------
+        System.out.println(c);   //          100 = 4 
+                                //  output is 4
+       
+    }
+}
