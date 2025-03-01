eb88f7ea939fef86f61d83d18957e3c8553b5eb6
DesaleRohit <rohitdesale2005@gmail.com>
2025-03-01 12:27:09 +0530

    [ADDED]:Rohit - This code for Bitwise XOR

---
+++ BitwiseXOR.java
@@ -0,0 +1,12 @@
+class BitwiseXOR {
+    public static void main(String[] args) {
+        int a = 5;             //Binary 101
+        int b = 10;            //Binary 010
+        int c = a ^ b;         //---------------
+        System.out.println(c); // Binary 111
+                             //Output is 15
+       
+       
+        }
+    }
+  
