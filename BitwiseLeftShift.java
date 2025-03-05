c400774a85aa22b222b59be76aff2407b6a3f790
DesaleRohit <rohitdesale2005@gmail.com>
2025-03-05 12:00:01 +0530

    [ADDED]:Rohit - This profram for Left Shift

---
+++ BitwiseLeftShift.java
@@ -0,0 +1,10 @@
+class BitwiseLeftShift {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(Integer.toBinaryString(a));
+        int b = a << 1;
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(b);
+    }
+}
+ 
