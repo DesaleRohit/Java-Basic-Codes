d5afd833556e2a896567a609e21488841625f088
DesaleRohit <rohitdesale2005@gmail.com>
2025-03-05 12:03:01 +0530

    [ADDED]:Rohit - This program for Right Shift

---
+++ BitwiseRightShift.java
@@ -0,0 +1,9 @@
+class BitwiseRightShift {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(Integer.toBinaryString(a));
+        int b = a >> 1;
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(b);
+    }
+}
