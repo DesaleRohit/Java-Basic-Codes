1b8706134d49ffa9e009b560884188b0bfbcf375
DesaleRohit <rohitdesale2005@gmail.com>
2025-03-06 08:17:23 +0530

    [ADDED]:Rohit - This program for Exception Handling

---
+++ ExceptionHandling.java
@@ -0,0 +1,14 @@
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        try {
+            int c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+            System.out.println("Cannot divide by zero");
+        }
+    }
+
+}
