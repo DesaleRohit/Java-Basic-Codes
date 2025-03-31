468ede7741b8cfe7425ad6d3bed670f21c96fe73
DesaleRohit <rohitdesale2005@gmail.com>
2025-03-31 10:35:44 +0530

    [ADDED]:Rohit - Interface inherited and implemented in class

---
+++ InterfaceDemo3.java
@@ -0,0 +1,25 @@
+interface A {
+    void fun1();
+}
+
+interface B extends A {
+    void fun2();
+}
+
+class C implements B {
+    public void fun1() {
+        System.out.println("This is Function 1");
+    }
+
+    public void fun2() {
+        System.out.println("This is Function 2");
+    }
+}
+
+public class InterfaceDemo3 {
+    public static void main(String[] args) {
+        C c = new C();
+        c.fun1();
+        c.fun2();
+    }
+}
