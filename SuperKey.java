ec4e9182987e0c491d1701de486ac107c7000e06
DesaleRohit <rohitdesale2005@gmail.com>
2025-04-29 20:02:36 +0530

    [ADDED]:Rohit - Super keyword use

---
+++ SuperKey.java
@@ -0,0 +1,19 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        super.sound();  // Calls the sound() method of Animal class
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}
