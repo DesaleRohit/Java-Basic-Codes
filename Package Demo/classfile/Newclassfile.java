427575574b62fbd48483ac38d4ca6278c375aa7c
DesaleRohit <rohitdesale2005@gmail.com>
2025-06-14 21:14:28 +0530

    [ADDED]:Rohit - for package practice

---
+++ Package Demo/classfile/Newclassfile.java
@@ -0,0 +1,7 @@
+package classfile;
+public class Newclassfile {
+    public void MSG() {
+         System.out.println("This msg from Newclassfile");
+}
+   
+   }
---
+++ Package Demo/objfile/Newclassfile2.java
@@ -0,0 +1,9 @@
+package objfile;
+import classfile.Newclassfile;
+
+public class Newclassfile2 {
+    public static void main(String[] args) {
+        Newclassfile newone = new Newclassfile();
+        newone.MSG();
+    }
+}
