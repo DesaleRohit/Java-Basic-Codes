756dec821013f5df565af85ac177fe75a820b888
DesaleRohit <rohitdesale2005@gmail.com>
2025-07-12 22:29:46 +0530

    [ADDED]:Rohit - Interface practice task

---
+++ Tasks/interface_Task.java
@@ -0,0 +1,42 @@
+interface Shape {
+    void area();
+}
+
+class Circle implements Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public void area() {
+        double pi = 3.14;
+        double result = pi * radius * radius;
+        System.out.println("Area of Circle: " + result);
+    }
+}
+
+class Rectangle implements Shape {
+    double length, width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public void area() {
+        double result = length * width;
+        System.out.println("Area of Rectangle: " + result);
+    }
+}
+
+public class interface_Task {
+    public static void main(String[] args) {
+      
+        Shape s1 = new Circle(5);         
+        Shape s2 = new Rectangle(4, 6); 
+
+        s1.area();  
+        s2.area();  
+    }
+}
