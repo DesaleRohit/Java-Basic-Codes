28cf4514c23d566734ca5bb06188abc5703b8d0c
DesaleRohit <rohitdesale2005@gmail.com>
2025-07-27 16:13:40 +0530

    [ADDED]:Rohit - NullPointerException demo

---
+++ nullPointerException.java
@@ -0,0 +1,12 @@
+public class nullPointerException {
+    public static void main(String[] args) {
+        String s = null;
+        try {
+             System.out.println(s.length());
+        } catch(NullPointerException e) {
+            System.out.println(e);
+            System.out.println("String value is "+e.getMessage());
+        }
+       
+    }
+}
