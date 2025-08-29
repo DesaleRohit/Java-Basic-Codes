7c488771fb3b6957e28a8481975a79ec4d1da2f4
DesaleRohit <rohitdesale2005@gmail.com>
2025-08-29 20:50:59 +0530

    [ADDED]:Rohit - Vector simple program

---
+++ Vectors/VectorDemo.java
@@ -0,0 +1,7 @@
+import java.util.Vector;
+public class VectorDemo {
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+        System.out.println("Default capacity of vector : "+vector.capacity());
+    }
+}
