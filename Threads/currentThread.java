1b11ed1216a22e5c621e43655b630ada83e3f7fd
DesaleRohit <rohitdesale2005@gmail.com>
2025-09-20 10:46:32 +0530

    [ADDED]:Rohit - current thread example

---
+++ Threads/currentThread.java
@@ -0,0 +1,6 @@
+public class currentThread {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+        System.out.println("Current Thread : "+thread);
+    }
+}
