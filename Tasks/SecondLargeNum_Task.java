75ce84f331fd00ff5d0c4ac1f1793003ed20cdac
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-11 15:21:52 +0530

    [ADDED]:Rohit - Second largest number logic using Arrays.sort()

---
+++ Tasks/SecondLargeNum_Task.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class SecondLargeNum_Task {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size :");
+        int n = sc.nextInt();
+        int arr [] = new int[n];
+        System.out.println("Enter Elements : ");
+        for(int i = 0; i < n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println("Second largest element : "+arr[n-2]);
+
+    }
+}
