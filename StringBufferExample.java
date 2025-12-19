f263e71e9137fc532df5dd24f53bbb25735048d1
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-19 16:46:16 +0530

    [ADDED]:Rohit - add basic StringBuffer append example

---
+++ StringBufferExample.java
@@ -0,0 +1,11 @@
+class StringBufferExample {
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer("IMRD");
+
+        sb.append(" College");
+        sb.append(" Shahada");
+
+        System.out.println(sb);
+    }
+}
