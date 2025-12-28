f9cc95c0423ebc7fc28a6d2b69e6d6aa73a24c9e
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-28 11:05:51 +0530

    [ADDED]:Rohit - ternary operator

---
+++ Revision/TernaryOperator.java
@@ -0,0 +1,10 @@
+class TernaryOperator {
+    public static void main(String[] args) {
+        
+        int a = 15;
+        int b = 20;
+
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum : "+max);
+    }
+}
