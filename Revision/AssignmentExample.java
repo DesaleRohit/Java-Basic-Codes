9b0ac9fd637a2d78f9f45f1215f05063e588852d
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-28 11:02:21 +0530

    [ADDED]:Rohit - Assignment operator practice

---
+++ Revision/AssignmentExample.java
@@ -0,0 +1,18 @@
+class AssignmentExample {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        
+        a += 5;
+        System.out.println("Now value of a is "+a);
+
+        a -= 3;
+        System.out.println("Now value of a is "+a);
+
+        a *= 2;
+        System.out.println("Now value of a is "+a);
+
+        a /= 4;
+        System.out.println("Now value of a is "+a);
+    }
+}
