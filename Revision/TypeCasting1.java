692f8b3cec6ae9cee223aceb23a953465dacc279
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-29 21:09:17 +0530

    [ADDED]:Rohit - Type casting int to double and double to int

---
+++ Revision/TypeCasting1.java
@@ -0,0 +1,7 @@
+class TypeCasting1 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;  //automatic casting
+        System.out.println("Afert casting "+b);
+    }
+}
---
+++ Revision/TypeCasting2.java
@@ -0,0 +1,7 @@
+class TypeCasting2 {
+    public static void main(String[] args) {
+        double x = 17.05;
+        int y = (int) x;
+        System.out.println("After casting "+y);
+    }
+}
