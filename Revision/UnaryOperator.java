98fea8be7300ad34c7ad94c7499c51d30383b729
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-29 20:58:36 +0530

    [ADDED]:Rohit - Pre-increament and Post-increament

---
+++ Revision/UnaryOperator.java
@@ -0,0 +1,8 @@
+class UnaryOperator {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(++x); // 6
+        System.out.println(x++); // 6
+        System.out.println(x);   // 7
+    }
+}
