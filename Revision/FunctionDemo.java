4fa65651378858c3c14770474c09b1d120ad8f9f
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-29 21:22:45 +0530

    [ADDED]:Rohit - Simple function demo

---
+++ Revision/FunctionDemo.java
@@ -0,0 +1,10 @@
+class FunctionDemo {
+    public static int addFunction(int x , int y) {
+        return x + y;
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Addition : "+addFunction(a , b));
+    }
+}
