2889b522af34b5c55060a3f63668136394c42092
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-30 18:45:19 +0530

    [ADDED]:Rohit - This code find large element in array

---
+++ Revision/MaxElement.java
@@ -0,0 +1,13 @@
+class MaxElement {
+    public static void main(String[] args) {
+        int [] arr = {50, 10, 45, 85, 74};
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Max element is :" +max);
+    }
+}
