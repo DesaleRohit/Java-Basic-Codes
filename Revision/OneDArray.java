b341eef43878daad01786f1c0f82d33a22df10c2
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-30 18:29:48 +0530

    [ADDED]:Rohit - One dimensional array example

---
+++ Revision/OneDArray.java
@@ -0,0 +1,8 @@
+class OneDArray {
+    public static void main(String[] args) {
+        int [] arr = { 20 , 78 , 45 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
