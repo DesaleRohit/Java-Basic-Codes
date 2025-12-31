d73bae0be259c15c963b6f633be36b9b1b6d017b
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-31 21:51:33 +0530

    [ADDED]:Rohit - Used toString()

---
+++ Revision/ArraytoString.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class ArraytoString {
+    public static void main(String[] args) {
+        int [] arr = {10 , 12 , 14};
+        System.out.println(Arrays.toString(arr));
+    }
+}
