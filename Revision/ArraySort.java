93a91a7edeb8609e88e71428741cf396a5fbe3d4
DesaleRohit <rohitdesale2005@gmail.com>
2025-12-31 21:52:38 +0530

    [ADDED]:Rohit  - Use sort()

---
+++ Revision/ArraySort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class ArraySort {
+    public static void main(String[] args) {
+        int [] arr = {45 , 12 , 44 , 87 , 54};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
