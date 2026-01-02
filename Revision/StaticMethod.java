1413bfb410c4a614f56d81901bdc676cb1d899b2
DesaleRohit <rohitdesale2005@gmail.com>
2026-01-02 18:39:25 +0530

    [ADDED]:Rohit - Static method demo

---
+++ Revision/StaticMethod.java
@@ -0,0 +1,11 @@
+class Test {
+    static void show() {
+        System.out.println("This method is Static");
+    }
+}
+
+public class StaticMethod {
+    public static void main(String[] args) {
+        Test.show();
+    }
+}
