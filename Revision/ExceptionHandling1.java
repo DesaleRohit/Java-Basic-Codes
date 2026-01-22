9ae9fd98ebc45e5a47b419f8f753856cd2ac90af
DesaleRohit <rohitdesale2005@gmail.com>
2026-01-22 21:35:20 +0530

    [ADDED]:Rohit - Basic try–catch Example

---
+++ Revision/ExceptionHandling1.java
@@ -0,0 +1,9 @@
+class ExceptionHandling1 {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
