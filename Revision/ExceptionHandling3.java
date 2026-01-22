7cea155146dfecd3298a074b75597ab15234432e
DesaleRohit <rohitdesale2005@gmail.com>
2026-01-22 21:36:52 +0530

    [ADDED]:Rohit - finally Block Example

---
+++ Revision/ExceptionHandling3.java
@@ -0,0 +1,10 @@
+class ExceptionHandling3 {
+    public static void main(String[] args) {
+        try {
+            int x = 10 / 2;
+            System.out.println(x);
+        } finally {
+            System.out.println("Finally block always executes");
+        }
+    }
+}
