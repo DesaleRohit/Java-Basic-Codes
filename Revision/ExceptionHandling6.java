7835e2e01d7ee87ba56dfdf16de9ee4c09128026
DesaleRohit <rohitdesale2005@gmail.com>
2026-01-26 21:04:40 +0530

    [ADDED]:Rohit - try-catch finally block

---
+++ Revision/ExceptionHandling6.java
@@ -0,0 +1,15 @@
+class ExceptionHandling6 {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+            int result = a / b;
+            System.out.println("Result: "+result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("Finally block executed");
+        }
+        System.out.println("Program continues");
+    }
+}
