ab5768c4b816235f9869e65a71268fae071af1f0
DesaleRohit <rohitdesale2005@gmail.com>
2026-01-26 21:13:06 +0530

    [ADDED]:Rohit - Bitwise operator

---
+++ Revision/BitwiseOperator.java
@@ -0,0 +1,11 @@
+class BitwiseOperator {
+    public static void main(String[] args) {
+
+        int a = 5;   // a = 0101
+        int b = 3;  //  b = 0011
+
+        System.out.println(a & b); // 1
+        System.out.println(a | b); // 7
+        System.out.println(a ^ b); // 6
+    }
+}
