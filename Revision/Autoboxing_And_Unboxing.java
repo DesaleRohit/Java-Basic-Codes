a2fa81feca2b055049004235927befb340e98f18
DesaleRohit <rohitdesale2005@gmail.com>
2026-02-06 20:34:16 +0530

    [ADDED]:Rohit - wrapper class use

---
+++ Revision/Autoboxing_And_Unboxing.java
@@ -0,0 +1,9 @@
+class Autoboxing_And_Unboxing {
+    public static void main(String[] args) {
+        Integer a = 10;  // Autoboxing
+        int b = a;      // Unboxing
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
