84a2f30bab9e1b633d0fe0518a63743669f4bb3d
DesaleRohit <rohitdesale2005@gmail.com>
2026-02-15 18:44:25 +0530

    [ADDED]:Rohit - List Interface using ArrayList practice

---
+++ Revision/List_Interface2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class List_Interface2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i= 0 ; i < 10 ; i++) {
+            list.add(i);
+        }
+
+        System.out.println(list);
+
+    }
+}
