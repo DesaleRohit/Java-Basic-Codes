087f8bdca62a98df8c5be799a98c1be1072b1e22
DesaleRohit <rohitdesale2005@gmail.com>
2026-05-29 00:38:16 +0530

    [ADDED]:Rohit - converting Array into List

---
+++ ArrayList/ArrayAsList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayAsList {
+    public static void main(String[] args) {
+        String[] s = { "1", "2", "3" };
+        List l = Arrays.asList(s);
+        System.out.println(l);
+
+    }
+}
