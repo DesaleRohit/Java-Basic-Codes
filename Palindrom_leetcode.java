7da27891711d4cb0f8475dc6fe800eb4214cacdd
DesaleRohit <rohitdesale2005@gmail.com>
2026-07-24 22:08:57 +0530

    [ADDED]:Rohit - leetcode to find palindrome number

---
+++ Palindrom_leetcode.java
@@ -0,0 +1,28 @@
+public class Palindrom_leetcode {
+    public static boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+
+        int number = 121;
+
+        boolean result = isPalindrome(number);
+
+        System.out.println(result);
+    }
+}
