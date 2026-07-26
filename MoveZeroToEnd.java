86e802121d99a08c37a950d1fa56379178dca56a
DesaleRohit <rohitdesale2005@gmail.com>
2026-07-26 18:07:46 +0530

    [ADDED]:Rohit - move zero to end of array

---
+++ MoveZeroToEnd.java
@@ -0,0 +1,27 @@
+public class MoveZeroToEnd {
+    public static void main(String[] args) {
+
+        int[] arr = { 0, 1, 0, 3, 12 };
+
+        int index = 0;
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        // Fill remaining positions with zero
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+
+        // Print array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
